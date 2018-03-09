0fa7fd3d075788b7b9d4803fc6eb728e8d663f1a
Jannis R <mail@jannisr.de>
2018-03-09 17:59:02 +0100

    improve repo structure

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 
 const path = require('path')
 
-const base = path.join(__dirname, 'lib', 'hello_world', 'target')
+const base = path.join(__dirname, 'target')
 const tail = path.join('release', 'hello_world.js')
 
 let sayHello
 if (Webassembly) {
-	const m = require(path.join(base, 'wasm32-unknown-emscripten', tail))
-	sayHello = m._say_hello
+	const dir = path.join(__dirname, 'target', 'wasm32-unknown-emscripten', 'release')
+	const module = require(path.join(dir, 'hello_world.js'))
+	sayHello = module._say_hello
 } else {
-	const m = require(path.join(base, 'asmjs-unknown-emscripten', tail))
-	sayHello = Module.cwrap('say_hello', '', [])
+	const dir = path.join(__dirname, 'target', 'asmjs-unknown-emscripten', 'release')
+	const module = require(path.join(dir, 'hello_world.js'))
+	sayHello = module.cwrap('say_hello', '', [])
 }
 
 module.exports = sayHello
